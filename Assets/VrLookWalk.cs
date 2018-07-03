7377995c6722a723637e70a7c46393f05a5124b6
michaelbridgette <c00205948@itcarlow.ie>
2018-07-03 16:01:42 +0100

    First commit

---
+++ Assets/VrLookWalk.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrLookWalk : MonoBehaviour {
+
+
+    public Transform vrCam;
+
+    public float toggleAngle = 30.0f;
+
+    public float speed = 3.0f;
+
+    public bool moveForward;
+
+    private CharacterController cc;
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(vrCam.eulerAngles.x >= toggleAngle&& vrCam.eulerAngles.x<90.0f)
+        {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+        }
+
+        if(moveForward)
+        {
+            Vector3 forward = vrCam.TransformDirection(Vector3.forward);
+
+            cc.SimpleMove(forward * speed);
+        }
+	}
+}
