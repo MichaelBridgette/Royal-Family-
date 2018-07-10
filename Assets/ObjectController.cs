0feb321b5f7a7020e7ed887723276f1f5db72ae9
michaelbridgette <c00205948@itcarlow.ie>
2018-07-10 11:53:09 +0100

    Game complete

--- Assets/ObjectController.cs
+++ Assets/ObjectController.cs
@@ -1,4 +1,4 @@
-// Copyright 2014 Google Inc. All rights reserved.
+/// Copyright 2014 Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,11 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace GoogleVR.HelloVR {
-  using UnityEngine;
+namespace GoogleVR.HelloVR
+{
+    using UnityEngine;
 
-  [RequireComponent(typeof(Collider))]
-  public class ObjectController : MonoBehaviour {
+    [RequireComponent(typeof(Collider))]
+    public class ObjectController : MonoBehaviour
+    {
 
         private Vector3 startingPosition;
         private Renderer myRenderer;
@@ -25,79 +27,92 @@ namespace GoogleVR.HelloVR {
         public Sprite inactiveMaterial;
         public Sprite gazedAtMaterial;
         public AudioSource clip;
+        public TextMesh text;
 
-    void Start() {
-      startingPosition = transform.localPosition;
-      myRenderer = GetComponent<Renderer>();
-      SetGazedAt(false);
-      clip.Stop();
-    }
+        void Start()
+        {
+            startingPosition = transform.localPosition;
+            myRenderer = GetComponent<Renderer>();
+            SetGazedAt(false);
+            clip.Stop();
+            text.gameObject.SetActive(false);
+        }
 
-    public void SetGazedAt(bool gazedAt) {
-      if (inactiveMaterial != null && gazedAtMaterial != null) {
-    //    myRenderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
-        this.GetComponent<SpriteRenderer>().sprite = gazedAt ? gazedAtMaterial : inactiveMaterial;
+        public void SetGazedAt(bool gazedAt)
+        {
+            if (inactiveMaterial != null && gazedAtMaterial != null)
+            {
+                //    myRenderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+                this.GetComponent<SpriteRenderer>().sprite = gazedAt ? gazedAtMaterial : inactiveMaterial;
 
-                if(gazedAt != lastGaze)
+                if (gazedAt != lastGaze)
                 {
-                    if(gazedAt == true)
+                    if (gazedAt == true)
                     {
                         clip.Play();
+                        text.gameObject.SetActive(true);
                     }
                     else
                     {
                         clip.Stop();
+                        text.gameObject.SetActive(false);
                     }
                 }
 
                 lastGaze = gazedAt;
-        return;
-      }
-    }
+                return;
+            }
+        }
 
-    public void Reset() {
-      int sibIdx = transform.GetSiblingIndex();
-      int numSibs = transform.parent.childCount;
-      for (int i=0; i<numSibs; i++) {
-        GameObject sib = transform.parent.GetChild(i).gameObject;
-        sib.transform.localPosition = startingPosition;
-        sib.SetActive(i == sibIdx);
-      }
-    }
+        public void Reset()
+        {
+            int sibIdx = transform.GetSiblingIndex();
+            int numSibs = transform.parent.childCount;
+            for (int i = 0; i < numSibs; i++)
+            {
+                GameObject sib = transform.parent.GetChild(i).gameObject;
+                sib.transform.localPosition = startingPosition;
+                sib.SetActive(i == sibIdx);
+            }
+        }
 
-    public void Recenter() {
+        public void Recenter()
+        {
 #if !UNITY_EDITOR
-      GvrCardboardHelpers.Recenter();
+            GvrCardboardHelpers.Recenter();
 #else
-      if (GvrEditorEmulator.Instance != null) {
-        GvrEditorEmulator.Instance.Recenter();
-      }
+            if (GvrEditorEmulator.Instance != null)
+            {
+                GvrEditorEmulator.Instance.Recenter();
+            }
 #endif  // !UNITY_EDITOR
+        }
+
+        public void TeleportRandomly()
+        {
+            // Pick a random sibling, move them somewhere random, activate them,
+            // deactivate ourself.
+            int sibIdx = transform.GetSiblingIndex();
+            int numSibs = transform.parent.childCount;
+            sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
+            GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
+
+            // Move to random new location ±100º horzontal.
+            Vector3 direction = Quaternion.Euler(
+                0,
+                Random.Range(-90, 90),
+                0) * Vector3.forward;
+            // New location between 1.5m and 3.5m.
+            float distance = 2 * Random.value + 1.5f;
+            Vector3 newPos = direction * distance;
+            // Limit vertical position to be fully in the room.
+            newPos.y = Mathf.Clamp(newPos.y, -1.2f, 4f);
+            randomSib.transform.localPosition = newPos;
+
+            randomSib.SetActive(true);
+            gameObject.SetActive(false);
+            SetGazedAt(false);
+        }
     }
-
-    public void TeleportRandomly() {
-      // Pick a random sibling, move them somewhere random, activate them,
-      // deactivate ourself.
-      int sibIdx = transform.GetSiblingIndex();
-      int numSibs = transform.parent.childCount;
-      sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
-      GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
-
-      // Move to random new location ±100º horzontal.
-      Vector3 direction = Quaternion.Euler(
-          0,
-          Random.Range(-90, 90),
-          0) * Vector3.forward;
-      // New location between 1.5m and 3.5m.
-      float distance = 2 * Random.value + 1.5f;
-      Vector3 newPos = direction * distance;
-      // Limit vertical position to be fully in the room.
-      newPos.y = Mathf.Clamp(newPos.y, -1.2f, 4f);
-      randomSib.transform.localPosition = newPos;
-
-      randomSib.SetActive(true);
-      gameObject.SetActive(false);
-      SetGazedAt(false);
-    }
-  }
 }
+
